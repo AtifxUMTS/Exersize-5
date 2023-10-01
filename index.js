431a6e42c767b5292212f02a0d487fb8d828b51d
AtifxUMTS <atifalishahnaqvi3345@gmail.com>
2023-10-01 03:44:08 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+"use strict";
+// famous Quote2: repeat exersize 4, but this time store famous person's name in veriable called famous_person. Then compose your message and store it in a new veriable called message. Print your message.
+// famous Quote: find a quote from famous person you admire. print the quote and the name of its author. your output should look somthing like the following, including the quotation mark:
+// Nelson Mandela once said "The greatest glory in living lies not in never falling, but in rising every time we fall."
+// Allama Iqbal said From your past emerges the present, and "from the present is born your future."
+//console.log('Allama Iqbal said From your past emerges the present, and "from the present is born your future."')
+let famous_person = " Allama Iqbal";
+let message = famous_person + ' "from the present is born your future."';
+console.log(message);
